1d5b70dc2990cf015ebbabc9ba7cae9de023d72b
Arush Khasru <ak3949235@gmail.com>
2025-12-27 08:24:12 +0530

    some changes

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -9,5 +9,5 @@ import "dotenv/config";
     RESEND_API_KEY : process.env.RESEND_API_KEY,
     EMAIL_FROM : process.env.EMAIL_FROM,
     EMAIL_FROM_NAME : process.env.EMAIL_FROM_NAME,
-    CLIENT_URL : "https://chat-app-nu9t.onrender.com"
+    CLIENT_URL : process.env.CLIENT_URL
 }
